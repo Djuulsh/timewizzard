d50bc8e8f14aee29cbda88c17fc2ea2a8cabb0b3
Djuulsh <36342137+Djuulsh@users.noreply.github.com>
2026-08-29 12:09:00 +0200

    Show v1.5.12 in Discord webbuilder reply

--- src/v158Support.js
+++ src/v158Support.js
@@ -5,7 +5,7 @@ import {
   MessageFlags
 } from 'discord.js';
 
-const VERSION = '1.5.11';
+const VERSION = '1.5.12';
 
 export function installV158Support(ControllerClass) {
   ControllerClass.prototype.showWebBuilder = async function showWebBuilderV158(interaction) {
